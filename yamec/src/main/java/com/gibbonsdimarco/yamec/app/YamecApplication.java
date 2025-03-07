e2e6221e6ef607951038ab5574063868a5937439
begibbons2021 <50278740+begibbons2021@users.noreply.github.com>
2025-03-06 19:18:58 -0500

    Initial Project Creation

---
+++ yamec/src/main/java/com/gibbonsdimarco/yamec/app/YamecApplication.java
@@ -0,0 +1,13 @@
+package com.gibbonsdimarco.yamec.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YamecApplication.class, args);
+    }
+
+}
---
+++ yamec/src/test/java/com/gibbonsdimarco/yamec/app/YamecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gibbonsdimarco.yamec.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
