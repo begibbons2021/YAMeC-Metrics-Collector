c3486c9a75c313ab63c92991f8e21a3aa9c4c9e9
begibbons2021 <50278740+begibbons2021@users.noreply.github.com>
2025-04-16 16:39:17 -0400

    Created DiskHardwareInformation data container class & MediaType enumerated values

---
+++ yamec/yamec-app/src/main/java/com/gibbonsdimarco/yamec/app/data/MediaType.java
@@ -0,0 +1,9 @@
+package com.gibbonsdimarco.yamec.app.data;
+
+public class MediaType {
+    public static final int UNSPECIFIED = 0;
+    public static final int HDD = 3;
+    public static final int SSD = 4;
+    public static final int SCM = 5;
+
+}
