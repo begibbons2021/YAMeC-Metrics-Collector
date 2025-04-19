4b5f712d506d978afbcde3e6ead3c10522f85d68
begibbons2021 <50278740+begibbons2021@users.noreply.github.com>
2025-04-19 10:02:33 -0400

    Added CpuHardwareInformation object class (Java)

---
+++ yamec/yamec-app/src/main/java/com/gibbonsdimarco/yamec/app/data/CpuHardwareInformation.java
@@ -0,0 +1,136 @@
+package com.gibbonsdimarco.yamec.app.data;
+
+/**
+ * Contains hardware information for CPUs connected to the System
+ */
+public class CpuHardwareInformation {
+
+    /**
+     * The name of the CPU this CpuHardwareInformation object pertains to as a human-readable
+     * string.
+     * <br>
+     * This is usually derived from the brand string of the CPU
+     */
+    private String friendlyName;
+
+    /**
+     * The number of physical cores (separate processors) which are present on the CPU
+     *  this CpuHardwareInformation object pertains to
+     */
+    private long coreCount;
+
+    /**
+     * The number of logical processors which are supported on the CPU this
+     * CpuHardwareInformation object pertains to.
+     * <br>
+     * Logical processors are usually facilitated through a technology such as
+     *   AMD Simultaneous Multithreading, SMT, or Intel Hyperthreading
+     */
+    private long logicalProcessorCount;
+
+    /**
+     * The architecture string of the CPU this CpuHardwareInformation object
+     * pertains to (i.e., x86, ARM, x64, etc.)
+     */
+    private String architecture;
+
+    /**
+     * The number of NUMA node groupings of CPU and memory present in the system
+     * and related to the CPU this CpuHardwareInformation object pertains to
+     */
+    private long numaNodeCount;
+
+    /**
+     * The number of bytes of level 1 cache present in the CPU this
+     * CpuHardwareInformation object pertains to
+     */
+    private long l1CacheSize;
+
+    /**
+     * The number of bytes of level 2 cache present in the CPU this
+     * CpuHardwareInformation object pertains to
+     */
+    private long l2CacheSize;
+
+    /**
+     * The amount of bytes of level 3 cache present in the CPU this
+     * CpuHardwareInformation object pertains to
+     */
+    private long l3CacheSize;
+
+    /**
+     * Whether the CPU this CpuHardwareInformation object pertains to support
+     * and has enabled virtualization technology (i.e., Windows Sandbox,
+     * Windows HyperV, etc.)
+     */
+    private boolean virtualizationEnabled;
+
+    /**
+     * Creates a new CpuHardwareInformation object instance from the hardware
+     * information passed by parameter
+     * @param friendlyName The name of the system's CPU as a String
+     * @param coreCount The number of cores present in the CPU
+     * @param logicalProcessorCount The number of threads supported by the CPU
+     * @param architecture The architecture of the CPU as a String
+     * @param numaNodeCount The number of NUMA nodes (groupings of CPU and Memory)
+     *                      present in the CPU
+     * @param l1CacheSize The number of bytes of level 1 cache in the CPU
+     * @param l2CacheSize The number of bytes of level 2 cache in the CPU
+     * @param l3CacheSize The number of bytes of level 3 cahce in the CPU
+     * @param virtualizationEnabled A boolean value representing whether hardware
+     *                              virtualization technology is supported by
+     *                              the CPU
+     */
+    public CpuHardwareInformation(String friendlyName,
+                                  long coreCount, long logicalProcessorCount,
+                                  String architecture,
+                                  long numaNodeCount,
+                                  long l1CacheSize, long l2CacheSize, long l3CacheSize,
+                                  boolean virtualizationEnabled) {
+        this.friendlyName = friendlyName;
+        this.coreCount = coreCount;
+        this.logicalProcessorCount = logicalProcessorCount;
+        this.architecture = architecture;
+        this.numaNodeCount = numaNodeCount;
+        this.l1CacheSize = l1CacheSize;
+        this.l2CacheSize = l2CacheSize;
+        this.l3CacheSize = l3CacheSize;
+        this.virtualizationEnabled = virtualizationEnabled;
+    }
+
+    public String getFriendlyName() {
+        return friendlyName;
+    }
+
+    public long getCoreCount() {
+        return coreCount;
+    }
+
+    public long getLogicalProcessorCount() {
+        return logicalProcessorCount;
+    }
+
+    public String getArchitecture() {
+        return architecture;
+    }
+
+    public long getNumaNodeCount() {
+        return numaNodeCount;
+    }
+
+    public long getL1CacheSize() {
+        return l1CacheSize;
+    }
+
+    public long getL2CacheSize() {
+        return l2CacheSize;
+    }
+
+    public long getL3CacheSize() {
+        return l3CacheSize;
+    }
+
+    public boolean isVirtualizationEnabled() {
+        return virtualizationEnabled;
+    }
+}
