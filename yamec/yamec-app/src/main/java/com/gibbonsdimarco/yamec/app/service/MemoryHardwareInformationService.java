0b849856e2703629271387b6944030d50bd8b420
begibbons2021 <50278740+begibbons2021@users.noreply.github.com>
2025-05-01 15:52:07 -0400

    Modified MemoryHardwareInformationService to include SystemMemoryMetricRepository as a member

--- yamec/yamec-app/src/main/java/com/gibbonsdimarco/yamec/app/service/MemoryHardwareInformationService.java
+++ yamec/yamec-app/src/main/java/com/gibbonsdimarco/yamec/app/service/MemoryHardwareInformationService.java
@@ -4,6 +4,7 @@ import com.gibbonsdimarco.yamec.app.data.CpuHardwareInformation;
 import com.gibbonsdimarco.yamec.app.data.MemoryHardwareInformation;
 import com.gibbonsdimarco.yamec.app.repository.CpuHardwareInformationRepository;
 import com.gibbonsdimarco.yamec.app.repository.MemoryHardwareInformationRepository;
+import com.gibbonsdimarco.yamec.app.repository.SystemMemoryMetricRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,12 +14,13 @@ import java.util.Objects;
 @Service
 public class MemoryHardwareInformationService {
 
-    private final MemoryHardwareInformationRepository memoryRepository;
+    private final SystemMemoryMetricRepository memoryMetricRepository;
     private final MemoryHardwareInformationRepository memoryHardwareInformationRepository;
 
     @Autowired
-    public MemoryHardwareInformationService(MemoryHardwareInformationRepository memoryRepository, MemoryHardwareInformationRepository memoryHardwareInformationRepository) {
-        this.memoryRepository = memoryRepository;
+    public MemoryHardwareInformationService(SystemMemoryMetricRepository memoryMetricRepository,
+                                            MemoryHardwareInformationRepository memoryHardwareInformationRepository) {
+        this.memoryMetricRepository = memoryMetricRepository;
         this.memoryHardwareInformationRepository = memoryHardwareInformationRepository;
     }
 
@@ -29,12 +31,13 @@ public class MemoryHardwareInformationService {
                 = memoryHardwareInformationRepository.findMatchingMemoryHardwareInformation(memoryInformation);
 
         // Returns the matching hardware object if it exists; otherwise, it saves it to the database
-        return Objects.requireNonNullElseGet(matchingConfiguration, () -> memoryRepository.save(memoryInformation));
+        return Objects.requireNonNullElseGet(matchingConfiguration,
+                () -> memoryHardwareInformationRepository.save(memoryInformation));
 
     }
 
     public java.util.List<MemoryHardwareInformation> getStoredMemoryInformation() {
-        return memoryRepository.findAll(
+        return memoryHardwareInformationRepository.findAll(
                 org.springframework.data.domain.PageRequest.of(0, 255,
                         org.springframework.data.domain.Sort.by(org.springframework.data.domain.Sort.Direction.DESC,
                                 "id")))
@@ -42,7 +45,7 @@ public class MemoryHardwareInformationService {
     }
 
     public java.util.List<MemoryHardwareInformation> getStoredMemoryInformation(int pageNumber) {
-        return memoryRepository.findAll(
+        return memoryHardwareInformationRepository.findAll(
                         org.springframework.data.domain.PageRequest.of(pageNumber, 255,
                                 org.springframework.data.domain.Sort.by(org.springframework.data.domain.Sort.Direction.DESC,
                                         "id")))
