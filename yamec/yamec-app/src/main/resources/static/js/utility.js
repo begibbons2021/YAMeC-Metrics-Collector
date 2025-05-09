f6c3cfed96cab58d4e774d512466ba8897d5d73b
begibbons2021 <50278740+begibbons2021@users.noreply.github.com>
2025-05-09 00:47:02 -0400

    Added additional functions in utility.js for formatting data from a raw form to a more readable form on the frontend

--- yamec/yamec-app/src/main/resources/static/js/utility.js
+++ yamec/yamec-app/src/main/resources/static/js/utility.js
@@ -20,6 +20,54 @@ function formatBytes(bytes, decimals = 2) {
 }
 
 /**
+ * Format seconds to human-readable format
+ *
+ * @param seconds The number of seconds to format
+ * @param decimals The number of decimal places to format to
+ * @returns {string} A string containing the number of bytes converted to an appropriately large unit
+ */
+function formatSeconds(seconds, decimals = 2) {
+
+    if (seconds === 0) return '0 ms';
+
+    const dm = decimals < 0 ? 0 : decimals;
+
+    if (seconds >= 1) {
+        return parseFloat(seconds).toFixed(dm) + "sec";
+    } else {
+        return (parseFloat(seconds)*1000.0).toFixed(dm) + "ms";
+    }
+
+}
+
+/**
+ * Format bytes to human-readable format
+ *
+ * @param bytes The number of bytes to format
+ * @param decimals The number of decimal places to format to
+ * @returns {string} A string containing the number of bytes converted to an appropriately large unit
+ */
+function formatBytesTransferred(bytes, decimals = 2) {
+    if (bytes === 0) return '0 Bytes/sec';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes/sec',
+                            'KB/sec',
+                            'MB/sec',
+                            'GB/sec',
+                            'TB/sec',
+                            'PB/sec',
+                            'EB/sec',
+                            'ZB/sec',
+                            'YB/sec'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
+
+/**
  * Format bits per second to human-readable format
  *
  * @param bps The number of bits to format
